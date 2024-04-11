62b8c10663d55c85e2874aa5dc2ef7e7b599a9a5
Israel Salinas <israel.salinas.m@gmail.com>
2024-04-10 21:36:51 -0600

    ejercicios string array

---
+++ ejerciciosStringArray/main.js
@@ -0,0 +1,10 @@
+/*
+    1.- Necesitamos poder entregar las iniciales del nombre de una persona, considerando únicamente primer nombre, apellido paterno y apellido materno
+    input => Israel Salinas Martínez
+    output => I. S. M.
+
+    2.- Necesitamos una ruleta de nombres, es decir, de una lista de nombres, debemos poder obtener algún nombre al azar. El nombre seleccionado debe ser eliminado de la lista original para poder volver a usar la ruleta.
+
+    Cuando ya no haya nombres, debe avisar que ya no hay nombres en la lista.
+
+   */
