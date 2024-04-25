ed7e17ed50c0255213b030714dce7d5e7f311906
Israel Salinas <israel.salinas.m@gmail.com>
2024-04-24 20:32:26 -0600

    ejercicio resuelto

--- ejercicioEventos2/js/main.js
+++ ejercicioEventos2/js/main.js
@@ -12,3 +12,49 @@
 
   3.- Agregar la funcionalidad para que cada que agregue una persona nueva, esta se vea reflejada junto con las anteriores en la interfaz de usuario
   */
+
+let createPersonButton = document.getElementById("create-person");
+
+let persons = [];
+
+createPersonButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("click");
+  let fields = document.querySelectorAll("#person-form input");
+  console.log(fields);
+
+  let personObject = {};
+
+  fields.forEach((field) => {
+    /*el nombre de la propiedad que voy a crear en mi objeto*/
+    let property = field.name;
+
+    /*el valor de la propiedad que voy a crear en el objeto*/
+    let value = field.value;
+
+    personObject[property] = value;
+  });
+
+  persons = [...persons, personObject];
+
+  printPersonsList(persons, "persons-list");
+});
+
+const createPersonItem = (personObject) => {
+  let { fullname } = personObject;
+  let personListItem = document.createElement("li");
+  personListItem.classList.add("list-group-item");
+  let personNameText = document.createTextNode(fullname);
+  personListItem.append(personNameText);
+  return personListItem;
+};
+
+const printPersonsList = (dataArray, wrapperId) => {
+  let wrapper = document.getElementById(wrapperId);
+
+  while (wrapper.firstChild) {
+    wrapper.removeChild(wrapper.firstChild);
+  }
+
+  dataArray.forEach((person) => wrapper.append(createPersonItem(person)));
+};
